6e50e49b40f622e03c36ce05439118a013567603
ajuntament-oliva <117634881+ajuntament-oliva@users.noreply.github.com>
2024-10-02 13:59:34 +0200

    Fent Actualitzar departament.php. Es borra el camp departament i no es deixa actualitzar.

--- add_departament.php
+++ add_departament.php
@@ -1,69 +1,69 @@
 <?php
-  $page_title = 'Add Group';
-  require_once('includes/load.php');
-  // Checkin What level user has permission to view this page
-   //page_require_level(1);
+$page_title = 'Add Group';
+require_once('includes/load.php');
+// Checkin What level user has permission to view this page
+//page_require_level(1);
 ?>
 <?php
-  if(isset($_POST['add'])){
+if (isset($_POST['add'])) {
 
-   $req_fields = array('departament-titol','nom-dispositiu', 'nom-persona', 'cognom-persona');
-   validate_fields($req_fields);
+  $req_fields = array('departament-titol', 'nom-dispositiu', 'nom-persona', 'cognom-persona');
+  validate_fields($req_fields);
 
-   if(empty($errors)){
-        $d_titol  = remove_junk($db->escape($_POST['departament-titol']));
-        $n_dispo  = remove_junk($db->escape($_POST['nom-dispositiu']));
-        $n_pers = remove_junk($db->escape($_POST['nom-persona']));
-        $cogn_pers  = remove_junk($db->escape($_POST['cognom-persona']));
+  if (empty($errors)) {
+    $d_titol = remove_junk($db->escape($_POST['departament-titol']));
+    $n_dispo = remove_junk($db->escape($_POST['nom-dispositiu']));
+    $n_pers = remove_junk($db->escape($_POST['nom-persona']));
+    $cogn_pers = remove_junk($db->escape($_POST['cognom-persona']));
 
-        $query  = "INSERT INTO departaments (";
-        $query .="departament, dispositiu, nom, cognom";
-        $query .=") VALUES (";
-        $query .=" '{$d_titol}', '{$n_dispo}','{$n_pers}','{$cogn_pers}' ";
-        $query .=")";
-        if($db->query($query)){
-          //sucess
-          $session->msg('s',"Has afegit un departament amb un dispositiu i la persona que el té! ");
-          redirect('add_departament.php', false);
-        } else {
-          //failed
-          $session->msg('d',"No s'ha pogut afegir nigun departament amb les seues dades.");
-          redirect('add_departament.php', false);
-        }
-   } else {
-     $session->msg("d", $errors);
-      redirect('add_departament',false);
-   }
- }
+    $query = "INSERT INTO departaments (";
+    $query .= "departament, dispositiu, nom, cognom";
+    $query .= ") VALUES (";
+    $query .= " '{$d_titol}', '{$n_dispo}','{$n_pers}','{$cogn_pers}' ";
+    $query .= ")";
+    if ($db->query($query)) {
+      //sucess
+      $session->msg('s', "Has afegit un departament amb un dispositiu i la persona que el té! ");
+      redirect('add_departament.php', false);
+    } else {
+      //failed
+      $session->msg('d', "No s'ha pogut afegir nigun departament amb les seues dades.");
+      redirect('add_departament.php', false);
+    }
+  } else {
+    $session->msg("d", $errors);
+    redirect('add_departament', false);
+  }
+}
 ?>
 <?php include_once('layouts/header.php'); ?>
 <div class="login-page">
-    <div class="text-center">
-       <h3>Afegix Departament</h3>
-     </div>
-     <?php echo display_msg($msg); ?>
-      <form method="post" action="add_departament.php" class="clearfix">
-        <div class="form-group">
-              <label for="name" class="control-label">Departament</label>
-              <input type="name" class="form-control" name="departament-titol">
-        </div>
-        <div class="form-group">
-              <label for="name" class="control-label">Dispositiu</label>
-              <input type="name" class="form-control" name="nom-dispositiu">
-        </div>
-        <div class="form-group">
-              <label for="name" class="control-label">Nom</label>
-              <input type="name" class="form-control" name="nom-persona">
-        </div>
-        <div class="form-group">
-              <label for="name" class="control-label">Cognom</label>
-              <input type="name" class="form-control" name="cognom-persona">
-        </div>
-        <div class="form-group clearfix">
-                <a href="departaments.php" class="btn btn-danger">Torna a Departaments</a>
-                <button type="submit" name="add" class="btn btn-info">Crea</button>
-        </div>
-    </form>
+  <div class="text-center">
+    <h3>Afegix Departament</h3>
+  </div>
+  <?php echo display_msg($msg); ?>
+  <form method="post" action="add_departament.php" class="clearfix">
+    <div class="form-group">
+      <label for="name" class="control-label">Departament</label>
+      <input type="name" class="form-control" name="departament-titol">
+    </div>
+    <div class="form-group">
+      <label for="name" class="control-label">Dispositiu</label>
+      <input type="name" class="form-control" name="nom-dispositiu">
+    </div>
+    <div class="form-group">
+      <label for="name" class="control-label">Nom</label>
+      <input type="name" class="form-control" name="nom-persona">
+    </div>
+    <div class="form-group">
+      <label for="name" class="control-label">Cognom</label>
+      <input type="name" class="form-control" name="cognom-persona">
+    </div>
+    <div class="form-group clearfix">
+      <a href="departaments.php" class="btn btn-danger">Torna a Departaments</a>
+      <button type="submit" name="add" class="btn btn-info">Crea</button>
+    </div>
+  </form>
 </div>
 
 <?php include_once('layouts/footer.php'); ?>
--- edit_departament.php
+++ edit_departament.php
@@ -1,82 +1,86 @@
 <?php
-  $page_title = 'Edit User';
-  require_once('includes/load.php');
-  // Checkin What level user has permission to view this page
-   //page_require_level(1);
+$page_title = 'Edit User';
+require_once('includes/load.php');
+// Checkin What level user has permission to view this page
+//page_require_level(1);
 ?>
 <?php
-  $e_departament = find_by_id('departaments',(int)$_GET['id']);
-  $groups  = find_all('departaments');
-  if(!$e_departament){
-    $session->msg("d","No es troba l'id del departament.");
-    redirect('departaments.php');
-  }
+$e_departament = find_by_id('departaments', (int) $_GET['id']);
+$groups = find_all('departaments');
+if (!$e_departament) {
+  $session->msg("d", "No es troba l'id del departament.");
+  redirect('departaments.php');
+}
 ?>
 
 <?php
 //Update Departament basic info
-if(isset($_POST['update'])) {
+if (isset($_POST['update'])) {
   $req_fields = array('departament-titol', 'dispositiu-nom', 'nom-persona', 'cognom-persona');
   validate_fields($req_fields);
-  if(empty($errors)){
-        $id = (int)$e_user['id'];
-        $departament = remove_junk($db->escape($_POST['departament-titol']));
-        $dispositiu = remove_junk($db->escape($_POST['dispositiu-nom']));
-        $nom = remove_junk($db->escape($_POST['nom-persona']));
-        $cognom = remove_junk($db->escape($_POST['cognom-persona']));
-          $sql = "UPDATE departaments SET departament ='{$departament}', dispositiu ='{$dispositiu}', nom='{$nom}', cognom='{$cognom}' WHERE id='{$db->escape($id)}'";
-       $result = $db->query($sql);
-        if($result && $db->affected_rows() === 1){
-          $session->msg('s',"Departament actualitzat! ");
-          redirect('edit_departament.php?id='.(int)$e_departament['id'], false);
-        } else {
-          $session->msg('d',"No s'ha pogut actualitzar!");
-          redirect('edit_departament.php?id='.(int)$e_departament['id'], false);
-        }
+  if (empty($errors)) {
+    $id = (int) $e_user['id'];
+    $departament = remove_junk($db->escape($_POST['departament-titol']));
+    $dispositiu = remove_junk($db->escape($_POST['dispositiu-nom']));
+    $nom = remove_junk($db->escape($_POST['nom-persona']));
+    $cognom = remove_junk($db->escape($_POST['cognom-persona']));
+    $sql = "UPDATE departaments SET departament ='{$departament}', dispositiu ='{$dispositiu}', nom='{$nom}', cognom='{$cognom}' WHERE id='{$db->escape($id)}'";
+    $result = $db->query($sql);
+    if ($result && $db->affected_rows() === 1) {
+      $session->msg('s', "Departament actualitzat! ");
+      redirect('edit_departament.php?id=' . (int) $e_departament['id'], false);
+    } else {
+      $session->msg('d', "No s'ha pogut actualitzar!");
+      redirect('edit_departament.php?id=' . (int) $e_departament['id'], false);
+    }
   } else {
     $session->msg("d", $errors);
-    redirect('edit_departament.php?id='.(int)$e_departament['id'],false);
+    redirect('edit_departament.php?id=' . (int) $e_departament['id'], false);
   }
 }
 ?>
 
 <?php include_once('layouts/header.php'); ?>
- <div class="row">
-   <div class="col-md-12"> <?php echo display_msg($msg); ?> </div>
-   <div class="col-md-3"></div>
+<div class="row">
+  <div class="col-md-12"> <?php echo display_msg($msg); ?> </div>
+  <div class="col-md-3"></div>
   <div class="col-md-6">
-     <div class="panel panel-default">
-       <div class="panel-heading">
+    <div class="panel panel-default">
+      <div class="panel-heading">
         <strong>
           <span class="glyphicon glyphicon-th"></span>
           Actualitzar <?php echo remove_junk(ucwords($e_departament['departament'])); ?> Departament
         </strong>
-       </div>
-       <div class="panel-body">
-          <form method="post" action="edit_departament.php?id=<?php echo (int)$e_departament['id'];?>" class="clearfix">
-            <div class="form-group">
-                  <label for="name" class="control-label">Departament</label>
-                  <input type="name" class="form-control" name="departament-titol" value="<?php echo remove_junk(ucwords($e_departament['departament'])); ?>">
-            </div>
-            <div class="form-group">
-                  <label for="name" class="control-label">Dispostiu</label>
-                  <input type="name" class="form-control" name="dispositiu-nom" value="<?php echo remove_junk(ucwords($e_departament['dispositiu'])); ?>">
-            </div>
-            <div class="form-group">
-                  <label for="name" class="control-label">Nom</label>
-                  <input type="name" class="form-control" name="nom-persona" value="<?php echo remove_junk(ucwords($e_departament['nom'])); ?>">
-            </div>
-            <div class="form-group">
-                  <label for="name" class="control-label">Cognom</label>
-                  <input type="name" class="form-control" name="cognom-persona" value="<?php echo remove_junk(ucwords($e_departament['cognom'])); ?>">
-            </div>
-            <div class="form-group clearfix">
-                    <button type="submit" name="update" class="btn btn-info">Actualitzar</button>
-            </div>
+      </div>
+      <div class="panel-body">
+        <form method="post" action="edit_departament.php?id=<?php echo (int) $e_departament['id']; ?>" class="clearfix">
+          <div class="form-group">
+            <label for="name" class="control-label">Departament</label>
+            <input type="name" class="form-control" name="departament-titol"
+              value="<?php echo remove_junk(ucwords($e_departament['departament'])); ?>">
+          </div>
+          <div class="form-group">
+            <label for="name" class="control-label">Dispostiu</label>
+            <input type="name" class="form-control" name="dispositiu-nom"
+              value="<?php echo remove_junk(ucwords($e_departament['dispositiu'])); ?>">
+          </div>
+          <div class="form-group">
+            <label for="name" class="control-label">Nom</label>
+            <input type="name" class="form-control" name="nom-persona"
+              value="<?php echo remove_junk(ucwords($e_departament['nom'])); ?>">
+          </div>
+          <div class="form-group">
+            <label for="name" class="control-label">Cognom</label>
+            <input type="name" class="form-control" name="cognom-persona"
+              value="<?php echo remove_junk(ucwords($e_departament['cognom'])); ?>">
+          </div>
+          <div class="form-group clearfix">
+            <button type="submit" name="update" class="btn btn-info">Actualitzar</button>
+          </div>
         </form>
-       </div>
-     </div>
+      </div>
+    </div>
   </div>
   <div class="col-md-3"></div>
- </div>
+</div>
 <?php include_once('layouts/footer.php'); ?>
