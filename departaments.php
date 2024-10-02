5b00ae6fb54c09ab22244b4ba52cf3bd9b3ea490
ajuntament-oliva <117634881+ajuntament-oliva@users.noreply.github.com>
2024-10-02 13:49:34 +0200

    Fent edit_departaments

--- departaments.php
+++ departaments.php
@@ -42,23 +42,12 @@
            <td><?php echo remove_junk(ucwords($a_departament['dispositiu']))?></td>
            <td><?php echo remove_junk(ucwords($a_departament['nom']))?></td>
            <td><?php echo remove_junk(ucwords($a_departament['cognom']))?></td>
-           <!--<td class="text-center">
-           <?php if($a_user['status'] === '1'): ?>
-            <span class="label label-success"><?php echo "Active"; ?></span>
-          <?php else: ?>
-            <span class="label label-danger"><?php echo "Deactive"; ?></span>
-          <?php endif;?>
-           </td>-->
-           <!--<td><?php echo read_date($a_user['last_login'])?></td>-->
            <td class="text-center">
              <div class="btn-group">
-                <a href="edit_user.php?id=<?php echo (int)$a_user['id'];?>" class="btn btn-xs btn-warning" data-toggle="tooltip" title="Edit">
+                <a href="edit_departament.php?id=<?php echo (int)$a_departament['id'];?>" class="btn btn-xs btn-warning" data-toggle="tooltip" title="Edit">
                   <i class="glyphicon glyphicon-pencil"></i>
                </a>
-                <!--<a href="delete_user.php?id=<?php echo (int)$a_user['id'];?>" class="btn btn-xs btn-danger" data-toggle="tooltip" title="Remove">
-                  <i class="glyphicon glyphicon-remove"></i>
-                </a>-->
-                </div>
+              </div>
            </td>
           </tr>
         <?php endforeach;?>
