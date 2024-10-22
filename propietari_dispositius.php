88f8104f7f066336e0dd3cc9ad27d9c1bb2898c7
ajuntament-oliva <117634881+ajuntament-oliva@users.noreply.github.com>
2024-10-22 09:47:21 +0200

    fent que els camps de data_creacio, hora_creacio, data_actualitzacio i hora_actualització es milloren

--- propietari_dispositius.php
+++ propietari_dispositius.php
@@ -7,12 +7,14 @@ $departament_id = isset($_GET['departament_id']) ? (int) $_GET['departament_id']
 if (isset($_GET['id'])) {
     $propietari_id = (int) $_GET['id'];
 
-    // Connexió BDA per obtenir els dispositius del propietari
-    $dispositius = $db->query("SELECT d.id, d.dispositiu 
-                                FROM dispositius d 
-                                JOIN dispositiu_propietari dp ON d.id = dp.dispositiu_id 
-                                WHERE dp.propietari_id = $propietari_id 
-                                ORDER BY d.dispositiu");
+    // Connexió BDA per obtenir els dispositius del propietari amb les seues característiques
+    $dispositius = $db->query("SELECT d.id, d.dispositiu, c.data_creacio, c.hora_creacio, c.data_actualitzacio, c.hora_actualitzacio
+                                    FROM dispositius d
+                                    JOIN dispositiu_propietari dp ON d.id = dp.dispositiu_id
+                                    JOIN caracteristiques_detalls c ON d.id = c.dispositiu_id
+                                    WHERE dp.propietari_id = $propietari_id
+                                    ORDER BY d.dispositiu");
+
 
     // Obtindre el nom del propietari
     $propietari = $db->query("SELECT nom, cognom 
@@ -45,25 +47,33 @@ if (isset($_GET['id'])) {
     <div class="col-md-6">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h4><?php echo remove_junk(ucwords($propietari['nom'] . ' ' . $propietari['cognom'])); ?></h4>
+                <h4><?php echo remove_junk(ucwords($propietari['nom'] . ' ' . $propietari['cognom'])); ?> - Llista de dispositius</h4>
             </div>
             <div class="panel-body">
                 <div class="table-responsive">
                     <table class="table table-bordered table-hover">
                         <thead>
                             <tr>
-                                <th>Llista de dispositius:</th>
+                                <th>Dispositius:</th>
+                                <th>Data creació:</th>
+                                <th>Hora creació:</th>
+                                <th>Data actualització:</th>
+                                <th>Hora actualització:</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php while ($dispositiu = $dispositius->fetch_assoc()): ?>
                                 <tr>
                                     <td><?php echo remove_junk(ucwords($dispositiu['dispositiu'])); ?></td>
+                                    <td><?php echo $dispositiu['data_creacio']; ?></td> 
+                                    <td><?php echo $dispositiu['hora_creacio']; ?></td>
+                                    <td><?php echo $dispositiu['data_actualitzacio']; ?></td>
+                                    <td><?php echo $dispositiu['hora_actualitzacio']; ?></td>
                                 </tr>
                             <?php endwhile; ?>
                             <?php if ($dispositius->num_rows == 0): ?>
                                 <tr>
-                                    <td colspan="1">No hi ha dispositius disponibles per a aquest propietari.</td>
+                                    <td colspan="3">No hi ha dispositius disponibles per a aquest propietari.</td>
                                 </tr>
                             <?php endif; ?>
                         </tbody>
@@ -72,7 +82,8 @@ if (isset($_GET['id'])) {
             </div>
             <div class="panel-footer">
                 <input type="hidden" name="departament_id" value="<?php echo $departament['departament_id']; ?>">
-                <a href="view_departament.php?id=<?php echo $departament['departament_id']; ?>" class="btn btn-danger">Torna enrere</a>
+                <a href="view_departament.php?id=<?php echo $departament['departament_id']; ?>"
+                    class="btn btn-danger">Torna enrere</a>
             </div>
         </div>
     </div>
